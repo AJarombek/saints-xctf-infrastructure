c15d9fa4fe345fae5abbae5fe2a362cbf677d69e
Andrew Jarombek <ajarombek95@gmail.com>
2019-02-13 17:58:26 -0500

    Work on Bastion Host for SSH connections

---
+++ bastion/bastion-key-gen.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Generate a key to connect to the bastion host.  This script must execute BEFORE the bastion host is created.
+# Author: Andrew Jarombek
+# Date: 2/13/2019
+
+# Used to preserve newlines in the private key.  Otherwise the key has an invalid format.
+# https://unix.stackexchange.com/a/164548
+IFS=
+
+# First delete any exiting key pair with the name "bastion-key"
+aws ec2 delete-key-pair --key-name bastion-key
+
+# Next create a new keypair.  Query the results back from AWS to get the private key
+BastionKey="$(aws ec2 create-key-pair --key-name bastion-key --query "KeyMaterial" --output text)"
+
+# In case needed, display the private key
+echo ${BastionKey}
+
+# Place the private key in a file called bastion-key.pem
+echo ${BastionKey} > ~/bastion-key.pem
+
+# Its recommended to change the private key permissions
+chmod 400 ~/bastion-key.pem
--- bastion/main.tf
+++ bastion/main.tf
@@ -4,6 +4,10 @@
  * Date: 2/12/2019
  */
 
+locals {
+  public_cidr = "0.0.0.0/0"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -23,37 +27,59 @@ data "aws_subnet" "saints-xctf-public-subnet-1" {
   }
 }
 
-resource "aws_instance" "bestion" {
+data "aws_vpc" "saints-xctf-vpc" {
+  tags {
+    Name = "SaintsXCTFcom VPC"
+  }
+}
+
+resource "aws_instance" "bastion" {
   # Use Amazon Linux 2
   ami = "ami-035be7bafff33b6b6"
 
   instance_type = "t2.micro"
-  subnet_id = "${data.aws_subnet.saints-xctf-public-subnet-1.id}"
+  key_name = "bastion-key"
+  associate_public_ip_address = true
 
-  network_interface {
-    device_index = 0
-    network_interface_id = ""
-    delete_on_termination = true
-  }
+  subnet_id = "${data.aws_subnet.saints-xctf-public-subnet-1.id}"
+  security_groups = ["${module.bastion-subnet-security-group.security_group_id}"]
 
   lifecycle {
     create_before_destroy = true
   }
+
+  tags {
+    Name = "Bastion Host"
+  }
 }
 
-resource "aws_network_interface" "bastion-network-interface" {
-  subnet_id = "${data.aws_subnet.saints-xctf-public-subnet-1.id}"
-  description = "Network Interface for the Bastion Host"
-  private_ips = ["10.0.2.0"]
-  private_ips_count = 1
-  security_groups = [""]
+module "bastion-subnet-security-group" {
+  source = "github.com/ajarombek/terraform-modules//security-group"
 
-  attachment {
-    device_index = 0
-    instance = "${aws_instance.bestion.id}"
-  }
-  
-  tags {
-    Name = "Network Interface for the Bastion Host"
-  }
+  # Mandatory arguments
+  name = "bastion-security"
+  tag_name = "Bastion Security Group"
+  vpc_id = "${data.aws_vpc.saints-xctf-vpc.id}"
+
+  # Optional arguments
+  sg_rules = [
+    # Inbound traffic for SSH
+    {
+      type = "ingress"
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = "${local.public_cidr}"
+    },
+    {
+      # Inbound traffic for ping
+      type = "ingress"
+      from_port = -1
+      to_port = -1
+      protocol = "icmp"
+      cidr_blocks = "${local.public_cidr}"
+    }
+  ]
+
+  description = "Allow SSH connections"
 }
