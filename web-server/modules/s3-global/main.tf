23bf3c84d7197d652ba1293dbfae61bc0e423eaa
Andrew Jarombek <ajarombek95@gmail.com>
2019-02-16 14:53:56 -0500

    Private S3 bucket policy

--- web-server/modules/s3-global/main.tf
+++ web-server/modules/s3-global/main.tf
@@ -6,6 +6,13 @@
 
 resource "aws_s3_bucket" "saints-xctf-credentials" {
   bucket = "saints-xctf-credentials"
+  policy = "${file("saints-xctf-credentials-policy.json")}"
+
+  cors_rule {
+    allowed_origins = ["*"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+  }
 
   tags {
     Name = "saints-xctf-credentials"
