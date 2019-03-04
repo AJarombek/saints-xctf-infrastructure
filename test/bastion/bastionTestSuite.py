cb75eb678bda2996dfbd8af8cbcd3c88e824483d
Andrew Jarombek <ajarombek95@gmail.com>
2019-03-03 19:56:52 -0500

    More work on Bastion test suite

--- test/bastion/bastionTestSuite.py
+++ test/bastion/bastionTestSuite.py
@@ -7,7 +7,11 @@ import masterTestFuncs as Test
 from bastion import bastionTestFuncs as Func
 
 tests = [
-    lambda: Test.test(Func.bastionec2running, "Bastion EC2 Instance Running")
+    lambda: Test.test(Func.bastion_ec2_running, "Bastion EC2 Instance Running"),
+    lambda: Test.test(Func.bastion_vpc, "Bastion VPC is SaintsXCTFcom"),
+    lambda: Test.test(Func.bastion_subnet, "Bastion Subnet is SaintsXCTFcom Public 1"),
+    lambda: Test.test(Func.bastion_rds_access, "Bastion has rds-access-role IAM role"),
+    lambda: Test.test(Func.bastion_security_group, "Bastion Security Group is bastion-security")
 ]
 
 
